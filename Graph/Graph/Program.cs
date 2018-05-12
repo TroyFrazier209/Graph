fa940bce5886e7725a2c9c8e45f49073380cc1c3
Troy Frazier <38478377+TroyFrazier209@users.noreply.github.com>
2018-05-11 21:36:44 -0500

    Add files via upload

--- Graph/Graph/Program.cs
+++ Graph/Graph/Program.cs
@@ -6,16 +6,18 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            ListGraph ports = new ListGraph("airport.txt");
+            ListGraph ports = new ListGraph("test.txt");
+            Console.WriteLine("Printing List...");
             Console.WriteLine(ports + "\n"); //Inital print
             ports.addNode("LON");
             ports.addEdge("LON", "MOB", 132);
             ports.addEdge("MOB", "LON", 132);
+            Console.WriteLine("Printing Depth-Search and Breath-Search...");
             ports.depthSearchPrint("LON");
             ports.breathSearchPrint("LON"); //Print of the graph with a new node
             Console.WriteLine();
             ports.removeNode("DFW"); 
-            Console.WriteLine(ports + "\n"); //New Print with DFW removed
+            Console.WriteLine("DFW Removed. Printing List...\n" + ports + "\n"); //New Print with DFW removed
             ports.depthSearchPrint("MOB"); //Follow two calls show that the list split into multiple graphs
             ports.breathSearchPrint("MOB");
             Console.WriteLine();
@@ -25,6 +27,7 @@ namespace Graph
             ports.addEdge("MOB", "LIT", 444);
             ports.addEdge("MOB", "SAT", 555);
             ports.addEdge("MOB", "SFO", 777);
+            Console.WriteLine("Printing Modified Graph in Depth-Seach, Breath-Search and Adj List...");
             ports.depthSearchPrint("MOB");
             ports.breathSearchPrint("MOB"); //Print of the graph reconnected into one graph
             ports.removeEdge("AUS", "SAT");
